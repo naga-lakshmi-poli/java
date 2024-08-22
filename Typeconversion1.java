122ad9e3f613572edaae913d7610c0fb90127b88
naga-lakshmi-poli <151710343+naga-lakshmi-poli@users.noreply.github.com>
2024-08-22 11:43:36 +0530

    Create Typeconversion1.java

---
+++ Typeconversion1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Typeconversion1
+{
+    public static void main(String args[])
+    {
+        double z;
+       //z=22/7;
+        z=(double) 22/7;
+        System.out.println("z value is"+z);
+    }
+}
