456f2fb239f4bd21dd7f1d4f1561318cf82d28b8
naga-lakshmi-poli <151710343+naga-lakshmi-poli@users.noreply.github.com>
2024-10-24 12:34:21 +0530

    Create Trycatch1.java

---
+++ Trycatch1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.Scanner;
+class Trycatch{
+    public static void main(String args[]){
+        int a=5,b=0,c;
+        c=a/b;
+        System.out.println("value of c is :" +c);
+    }
+}
